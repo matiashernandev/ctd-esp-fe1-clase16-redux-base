01892e8f3cba6589f1d9c69bf0010def11ece1f0
nkwaaaa <nkwaaaa@gmail.com>
2023-03-22 12:56:22 -0300

    update

--- src/components/BuscarPokemon.tsx
+++ src/components/BuscarPokemon.tsx
@@ -1,26 +1,43 @@
 import React, { useState } from "react";
+import { Pokemon } from "../types/pokemon.types";
 import ListadoPokemons from "./ListadoPokemons";
 import VistaPokemon from "./VistaPokemon";
 
 const BuscarPokemon = () => {
 	//Aqui deberemos almacenar en estados las entradas del usuario
-	useState;
+	const [inputPokemonName, setInputPokemonName] = useState("");
+	const [name, setName] = useState("");
+	const [pokemonSeleccionado, setPokemonSeleccionado] = useState<
+		Pokemon | undefined
+	>();
 
 	const onBuscarClick = () => {
 		// Aqui debemos guardar la entrada del usuario
+		setName(inputPokemonName);
+		console.log(inputPokemonName);
+	};
+
+	const seleccionarPokemon = (pokemon: Pokemon) => {
+		setPokemonSeleccionado(pokemon);
 	};
 
 	return (
 		<>
 			<div id="buscarPokemon">
 				<label>Buscar pokemon</label>
-				<input type="text" placeholder={"Pikachu, Charmander, Ditto, etc"} />
+				<input
+					onChange={(e) => {
+						setInputPokemonName(e.target.value);
+					}}
+					type="text"
+					placeholder={"Pikachu, Charmander, Ditto, etc"}
+				/>
 				<button onClick={() => onBuscarClick()}>Buscar</button>
 			</div>
 			<div style={{ display: "flex", flexDirection: "row" }}>
 				{/* ListadoPokemons debe recibir por props el name del pokemon a buscar */}
-				<ListadoPokemons />
-				<VistaPokemon />
+				<ListadoPokemons name={name} seleccionarPokemon={seleccionarPokemon} />
+				<VistaPokemon pokemonSeleccionado={pokemonSeleccionado} />
 			</div>
 		</>
 	);
--- src/components/ListadoPokemons.tsx
+++ src/components/ListadoPokemons.tsx
@@ -1,9 +1,8 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import ListadoPokemonsItem from "../components/ListadoPokemonsItem";
-import {buscarPokemons} from "../queries/pokemon.queries";
-import {Pokemon} from "../types/pokemon.types";
-import {extractPokemonId} from "../services/pokemon.services";
-
+import { buscarPokemons } from "../queries/pokemon.queries";
+import { Pokemon } from "../types/pokemon.types";
+import { extractPokemonId } from "../services/pokemon.services";
 
 /**
  * Visualiza una lista de pokemons
@@ -16,29 +15,42 @@ import {extractPokemonId} from "../services/pokemon.services";
  * @param seleccionarPokemon una funcion que se ejecuta al hacer click en el pokemon y guarda en un estado el pokemon seleccionado
  * @author Digital House
  */
-const ListadoPokemons = () => {
-    const [isLoading, setLoading] = useState(true);
-    const [pokemons, setPokemons] = useState<Pokemon[] | null>(null);
 
-    useEffect(() => {
-        //Deberan realizar la busqueda con la variable correspondiente
-        buscarPokemons("").then(data => {
-            setLoading(false);
-            setPokemons(data);
-        });
-    },[])
-
-    if (isLoading) return <div>Cargando pokemons...</div>
-
-    return (
-        <div id="listadoCategorias">
-            {pokemons && pokemons.map((pokemon: Pokemon) => (
-                <ListadoPokemonsItem pokemon={pokemon}
-                                     seleccionarPokemon={() => {}}
-                                     key={extractPokemonId(pokemon.url)}/>
-            ))}
-        </div>
-    );
+interface ListadoPokemonsProps {
+	name: string;
+	seleccionarPokemon: (pokemon: Pokemon) => void;
 }
+
+const ListadoPokemons = ({
+	name,
+	seleccionarPokemon,
+}: ListadoPokemonsProps) => {
+	const [isLoading, setLoading] = useState(true);
+	const [pokemons, setPokemons] = useState<Pokemon[] | null>(null);
+	console.log("🚀 ~ file: ListadoPokemons.tsx:30 ~ pokemons:", pokemons);
+
+	useEffect(() => {
+		//Deberan realizar la busqueda con la variable correspondiente
+		buscarPokemons(name).then((data) => {
+			setLoading(false);
+			setPokemons(data);
+		});
+	}, [name]);
+
+	if (isLoading) return <div>Cargando pokemons...</div>;
+
+	return (
+		<div id="listadoCategorias">
+			{pokemons &&
+				pokemons.map((pokemon: Pokemon) => (
+					<ListadoPokemonsItem
+						pokemon={pokemon}
+						seleccionarPokemon={seleccionarPokemon}
+						key={extractPokemonId(pokemon.url)}
+					/>
+				))}
+		</div>
+	);
+};
 
 export default ListadoPokemons;
